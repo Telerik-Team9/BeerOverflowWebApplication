8350144608de328cd5fed1bb3018e96b6ab0e1a7
Magdalena Nikolova <magi.nikolova@gmail.com>
2020-09-30 21:20:52 +0300

    Initial commit

---
+++ BeerOverflow/BeerOverflow.Models/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BeerOverflow.Models
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
