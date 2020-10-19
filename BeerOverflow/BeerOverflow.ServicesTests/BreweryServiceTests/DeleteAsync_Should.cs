781bf51c4a27c7e1ddbd57a285528bf1f447b623
Ali Marekov <a.marekov@outlook.com>
2020-10-20 01:24:34 +0300

    Add BreweryService DeleteAsync Method unit tests

---
+++ BeerOverflow/BeerOverflow.ServicesTests/BreweryServiceTests/DeleteAsync_Should.cs
@@ -0,0 +1,55 @@
+﻿using BeerOverflow.Database;
+using BeerOverflow.Services.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeerOverflow.ServicesTests.BreweryServiceTests
+{
+    [TestClass]
+    public class DeleteAsync_Should
+    {
+        [TestMethod]
+        public async Task DeleteBreweryWhen_ValidParams()
+        {
+            //Arrange
+            var options = Utils.GetOptions(Guid.NewGuid().ToString());
+            var brewery = Utils.GetBreweries().First();
+
+            using (var arrangeContext = new BeerOverflowDbContext(options))
+            {
+                await arrangeContext.Breweries.AddAsync(brewery);
+                arrangeContext.SaveChanges();
+            }
+
+            //Act & Assert
+            using (var actContext = new BeerOverflowDbContext(options))
+            {
+                var sut = new BreweryService(actContext);
+                var actual = await sut.DeleteAsync(brewery.Id);
+
+                Assert.IsTrue(actual);
+                Assert.IsTrue(actContext.Breweries.Any());
+            }
+        }
+
+        [TestMethod]
+        public async Task ReturnFalseWhen_NoSuchBrewery()
+        {
+            //Arrange
+            var options = Utils.GetOptions(Guid.NewGuid().ToString());
+
+            //Act & Assert
+            using (var actContext = new BeerOverflowDbContext(options))
+            {
+                var sut = new BreweryService(actContext);
+                var actual = await sut.DeleteAsync(Guid.NewGuid());
+
+                Assert.IsFalse(actual);
+            }
+        }
+    }
+}
