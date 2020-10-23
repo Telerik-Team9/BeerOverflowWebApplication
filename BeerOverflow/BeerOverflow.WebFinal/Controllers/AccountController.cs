bcb9588dcfeb21a60d0481d107f970c0b5c79096
Ali Marekov <a.marekov@outlook.com>
2020-10-23 20:25:20 +0300

    Add custom Account Controller & Login view

---
+++ BeerOverflow/BeerOverflow.WebFinal/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using BeerOverflow.Models;
+using BeerOverflow.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BeerOverflow.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<User> signInManager;
+
+        public AccountController(SignInManager<User> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LogInViewModel item)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await this.signInManager
+                    .PasswordSignInAsync(item.Email, item.Password, isPersistent: true, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            return RedirectToAction("Error", "Home");
+        }
+    }
+}
---
+++ BeerOverflow/BeerOverflow.WebFinal/Models/LogInViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BeerOverflow.Web.Models
+{
+    public class LogInViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
