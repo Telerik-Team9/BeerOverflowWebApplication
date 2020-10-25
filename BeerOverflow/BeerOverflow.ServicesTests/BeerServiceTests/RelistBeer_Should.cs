0296c457bffa66b7b06b77f3a6b20a346bf579a0
Ali Marekov <a.marekov@outlook.com>
2020-10-25 23:46:09 +0200

    Add BeerService Unlist?Relist methods unnit tests

---
+++ BeerOverflow/BeerOverflow.ServicesTests/BeerServiceTests/RelistBeer_Should.cs
@@ -0,0 +1,53 @@
+﻿using BeerOverflow.Database;
+using BeerOverflow.Services.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeerOverflow.ServicesTests.BeerServiceTests
+{
+    [TestClass]
+    public class RelistBeer_Should
+    {
+        [TestMethod]
+        public async Task RelistBeerWhen_ValidParams()
+        {
+            //Arrange
+            var options = Utils.GetOptions(Guid.NewGuid().ToString());
+            var beers = Utils.GetBeers();
+            var toRelist = beers.Last();
+            toRelist.IsUnlisted = true;
+
+            using (var arrangeContext = new BeerOverflowDbContext(options))
+            {
+                await arrangeContext.Beers.AddRangeAsync(beers);
+                await arrangeContext.SaveChangesAsync();
+            }
+
+            //Act & Assert
+            using (var assertContext = new BeerOverflowDbContext(options))
+            {
+                var sut = new BeerService(assertContext);
+                var result = await sut.RelistBeer(toRelist.Id);
+
+                Assert.IsTrue(result);
+            }
+        }
+
+        [TestMethod]
+        public async Task ThrowWhen_BeerNotFound()
+        {
+            var options = Utils.GetOptions(Guid.NewGuid().ToString());
+
+            //Act & Assert
+            using (var actContext = new BeerOverflowDbContext(options))
+            {
+                var sut = new BeerService(actContext);
+                await Assert.ThrowsExceptionAsync<ArgumentException>(async () => await sut.RelistBeer(Guid.Empty));
+            }
+        }
+    }
+}
---
+++ BeerOverflow/BeerOverflow.ServicesTests/BeerServiceTests/UnlistBeer_Should.cs
@@ -0,0 +1,52 @@
+﻿using BeerOverflow.Database;
+using BeerOverflow.Services.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeerOverflow.ServicesTests.BeerServiceTests
+{
+    [TestClass]
+    public class UnlistBeer_Should
+    {
+        [TestMethod]
+        public async Task UnlistBeerWhen_ValidParams()
+        {
+            //Arrange
+            var options = Utils.GetOptions(Guid.NewGuid().ToString());
+            var beers = Utils.GetBeers();
+            var toUnlist = beers.Last();
+
+            using(var arrangeContext = new BeerOverflowDbContext(options))
+            {
+                await arrangeContext.Beers.AddRangeAsync(beers);
+                await arrangeContext.SaveChangesAsync();
+            }
+
+            //Act & Assert
+            using(var assertContext = new BeerOverflowDbContext(options))
+            {
+                var sut = new BeerService(assertContext);
+                var result = await sut.UnlistBeer(toUnlist.Id);
+
+                Assert.IsTrue(result);
+            }
+        }
+
+        [TestMethod]
+        public async Task ThrowWhen_BeerNotFound()
+        {
+            var options = Utils.GetOptions(Guid.NewGuid().ToString());
+
+            //Act & Assert
+            using (var actContext = new BeerOverflowDbContext(options))
+            {
+                var sut = new BeerService(actContext);
+                await Assert.ThrowsExceptionAsync<ArgumentException>(async () => await sut.UnlistBeer(Guid.Empty));
+            }
+        }
+    }
+}
