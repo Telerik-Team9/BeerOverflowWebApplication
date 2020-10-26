2163db7c6acd2d2ddfdc7dec46ebfead43c1614f
Magdalena Nikolova <magi.nikolova@gmail.com>
2020-10-27 01:28:10 +0200

    Add Carousel and modify Beer Search table

--- BeerOverflow/BeerOverflow.WebFinal/wwwroot/js/site.js
+++ BeerOverflow/BeerOverflow.WebFinal/wwwroot/js/site.js
@@ -2,3 +2,29 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+var $item = $('.carousel-item');
+var $wHeight = $(window).height();
+$item.eq(0).addClass('active');
+$item.height($wHeight);
+$item.addClass('full-screen');
+
+$('.carousel img').each(function () {
+    var $src = $(this).attr('src');
+    var $color = $(this).attr('data-color');
+    $(this).parent().css({
+        'background-image': 'url(' + $src + ')',
+        'background-color': $color
+    });
+    $(this).remove();
+});
+
+$(window).on('resize', function () {
+    $wHeight = $(window).height();
+    $item.height($wHeight);
+});
+
+$('.carousel').carousel({
+    interval: 6000,
+    pause: "false"
+});
